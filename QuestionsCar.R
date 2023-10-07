e49dd8f749ed57c9a8cabd62dcff88c745f80e84
MazingerX <lehinostroza@icloud.com>
2023-10-06 22:52:51 -0500

    all questions answered

--- QuestionsCar.R
+++ QuestionsCar.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(car)
 library(dplyr)
 library(ggplot2)
+library(vcd)
 
 
 str(Auto)
@@ -15,11 +16,11 @@ data1 <- rnorm(1000)
 hist(data1, main = "Histogram of data", xlab = "Data Values")
 
 qqnorm(data1)
-
 shapiro.test(data1)
 
-library(ggplot2)
-ggplot(Auto, aes(x = horsepower, y = acceleration)) +
+---
+hp_acce <- select(Auto, horsepower, acceleration)
+ggplot(hp_acce, aes(x = horsepower, y = acceleration)) +
   geom_point() +
   geom_smooth(method = "lm", se = FALSE, color = "red") +
   labs(title = "Relationship between Horsepower and Acceleration",
@@ -29,6 +30,57 @@ ggplot(Auto, aes(x = horsepower, y = acceleration)) +
 
 cor(Auto$horsepower, Auto$acceleration)
 
-data_hp_acce1 <- rnorm(1000)
-qqnorm(data_hp_acce1)
-shapiro.test(data_hp_acce1)
+---
+mpg_cyl <- select(Auto, mpg, cylinders)
+ggplot(mpg_cyl, aes(x = as.factor(cylinders), y = mpg)) +
+  geom_boxplot() +
+  labs(title = "Relationship between MPG and Cylinders",
+       x = "Number of Cylinders",
+       y = "MPG") +
+  theme_minimal()
+
+Auto %>%
+  group_by(cylinders) %>%
+  summarise(
+    mean_mpg = mean(mpg),
+    median_mpg = median(mpg),
+    sd_mpg = sd(mpg),
+    count = n()
+  )
+
+anova_result <- aov(mpg ~ as.factor(cylinders), data = Auto)
+summary(anova_result)
+
+---
+table_cyl_origin <- table(Auto$cylinders, Auto$origin)
+print(table_cyl_origin)
+mosaicplot(table_cyl_origin, main="Mosaic Plot of Cylinders and Origin", xlab="Cylinders", ylab="Origin", col=c("orange", "pink", "red"))
+chi_sq_test <- chisq.test(table_cyl_origin)
+print(chi_sq_test)
+
+ggplot(Auto, aes(x = displacement, y = weight)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE, color = "red") +
+  labs(title = "Relationship between Displacement and Weight",
+       x = "Displacement",
+       y = "Weight") +
+  theme_minimal()
+
+cor(Auto$displacement, Auto$weight)
+
+---
+table_origin <- table(Auto$origin)
+prop_origin <- prop.table(table_origin) * 100  # Convert to percentage
+print(table_origin)
+print(prop_origin)
+
+barplot(prop_origin,
+        main = "Percentage of Observations by Origin",
+        xlab = "Origin",
+        ylab = "Percentage",
+        col = c("red", "green", "blue"),
+        ylim = c(0, 100))
+
+---
+
+
