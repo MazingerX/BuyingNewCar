ee01ca4bfc481bbd0884df95956b63f76ad64a19
MazingerX <lehinostroza@icloud.com>
2023-10-06 20:26:24 -0500

    Working on Fourth question

--- QuestionsCar.R
+++ QuestionsCar.R
@@ -2,11 +2,14 @@ library(ISLR)
 library(tidyverse)
 library(car)
 library(dplyr)
+library(ggplot2)
 
 
 str(Auto)
 View(Auto)
 # Use Q-Q Plot and Shapiro-Wilk test to make sure is normal
+
+pairs(Auto)
 
 data1 <- rnorm(1000)
 hist(data1, main = "Histogram of data", xlab = "Data Values")
@@ -15,5 +18,17 @@ qqnorm(data1)
 
 shapiro.test(data1)
 
-data_hp_acce <- select(Auto, horsepower, acceleration)
-ggplot(data_hp_acce, aes(horsepower, acceleration)) + geom_point() + stat_smooth(method = lm)
+library(ggplot2)
+ggplot(Auto, aes(x = horsepower, y = acceleration)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE, color = "red") +
+  labs(title = "Relationship between Horsepower and Acceleration",
+       x = "Horsepower",
+       y = "Acceleration")
+
+
+cor(Auto$horsepower, Auto$acceleration)
+
+data_hp_acce1 <- rnorm(1000)
+qqnorm(data_hp_acce1)
+shapiro.test(data_hp_acce1)
